e2f32b98a1f3cd11718cf19cefc51f47a20ecd91
Dinesh Kumar <mapmydestination@gmail.com>
2025-04-18 02:09:39 +0530

    update

---
+++ Job_Portal/Job prtl/jobportal/slider.php
@@ -0,0 +1,13 @@
+ <link href="themes/1/js-image-slider.css" rel="stylesheet" type="text/css" />
+    <script src="themes/1/js-image-slider.js" type="text/javascript"></script>
+    <link href="generic.css" rel="stylesheet" type="text/css" />
+
+<div style="width:1048px; height:300px; border:solid 1px #D2D2FF;">
+
+<div id="slider" style="width:1048px; height:300px;border:solid 0px #FFF;" align="center";>
+<img src="images/wrkr.jpg"  height="345" width="1047"/>
+<img src="images/jobb.jpg"  />
+<img src="images/job3.jpg"  />
+	   </div>
+
+       </div>
